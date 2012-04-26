28a8b617fd926a355bb25d92dfbb863508f3ce1b
Jonathan Boyett <jonathan@failingservers.com>
2012-04-26 09:20:45 -0700

    reduce noisiness of corosync_status fact

--- lib/facter/corosync_status.rb
+++ lib/facter/corosync_status.rb
@@ -1,7 +1,7 @@
 # Oh, hai. This fact is used to get around http://projects.puppetlabs.com/issues/7165
 Facter.add(:corosync_status) do
   setcode do
-    if `service corosync status` =~ /running/
+    if File.exists?('/etc/init.d/corosync') && `service corosync status` =~ /running/
       'running'
     else
       'stopped'
